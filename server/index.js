1f0a7142b798c64809707c36f223bd71294753a8
Aatti13 <aattreya.ks@gmail.com>
2025-11-15 21:12:23 +0530

    Dependency imports and changes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import { connectDB } from './src/config/database.js';
+
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.listen(PORT, ()=>{
+  connectDB();
   console.log(`Listening at: http://localhost:${PORT}`)
 });
---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,4 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/user.model.js';
+
